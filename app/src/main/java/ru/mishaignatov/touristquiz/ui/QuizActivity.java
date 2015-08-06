0c97fe7e4f26b65fa368fde098c4e98c5c918c05
Ignatov <ruwinmike@gmail.com>
2015-08-06 18:10:37 +0300

    add Toast

--- app/src/main/java/ru/mishaignatov/touristquiz/ui/QuizActivity.java
+++ app/src/main/java/ru/mishaignatov/touristquiz/ui/QuizActivity.java
@@ -2,7 +2,6 @@ package ru.mishaignatov.touristquiz.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
